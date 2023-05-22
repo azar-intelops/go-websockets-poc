bbcb8ebf281916c4ca8fa6059ce5c5c2b63ed707
azar-intelops <amohammed@appstekcorp.com>
2023-05-22 12:23:04 +0530

    made client code

--- main.go
+++ main.go
@@ -8,52 +8,71 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool { return true },
-}
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home Page")
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		log.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-
-func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-
-	ws, err := upgrader.Upgrade(w, r, nil)
-
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("Client Connected Successfully ....")
-	reader(ws)
-}
-
-func setupRoutes() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/ws", wsEndpoint)
-
+// Message represents a message from a client
+type Message struct {
+	Username string `json:"username"`
+	Content  string `json:"content"`
 }
 
 func main() {
-	setupRoutes()
-	log.Println("Running it on => http://localhost:8080", )
+	// Serve the WebSocket endpoint
+	http.HandleFunc("/ws", handleConnections)
+
+	// Start listening for incoming messages to broadcast
+	go handleMessages()
+
+	// Start the server on localhost:8080
+	fmt.Println("Server started on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP connection to a WebSocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Close the WebSocket connection when the function returns
+	defer ws.Close()
+
+	// Register the new client
+	clients[ws] = true
+
+	for {
+		var msg Message
+
+		// Read a new message from the WebSocket connection
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("error: %v", err)
+			delete(clients, ws)
+			break
+		}
+
+		// Send the received message to the broadcast channel
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		// Get the next message from the broadcast channel
+		msg := <-broadcast
+
+		// Send the message to all connected clients
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("error: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
 }
